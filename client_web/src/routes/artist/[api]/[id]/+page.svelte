59015b09ecc3b28ee7233632ef923b066de23e66
DimitriLaPoudre <lou.pellegrino@epitech.eu>
2025-10-26 05:01:52 +0100

    feat: client_web /artist EPs and Singles section

--- client_web/src/routes/artist/[api]/[id]/+page.svelte
+++ client_web/src/routes/artist/[api]/[id]/+page.svelte
@@ -59,27 +59,80 @@
 	</div>
 	<!-- page body -->
 	<div>
-		<div style="display: flex; flex-wrap: wrap; gap: 10px;">
-			{#each artist.Albums as album}
-				<button
-					onclick={() => {
-						goto(`/album/${page.params.api}/${album.Id}`);
-					}}
-					style="display: flex; flex-direction: column; white-space: nowrap; overflow: hidden; text-overflow: ellipsis; width:160px; height: 200px;"
-				>
-					<img
-						src={album.CoverUrl}
-						alt={album.Title}
-						style="max-width:100%; max-height:100%;"
-					/>
-					<p>{album.Title}</p>
-					{#each album.Artists as artist}
-						<a href="/artist/{page.params.api}/{artist.Id}">
-							{artist.Name}
-						</a>
-					{/each}
-				</button>
-			{/each}
+		<div>
+			<h2>Albums</h2>
+			<div style="display: flex; flex-wrap: wrap; gap: 10px;">
+				{#each artist.Albums as album}
+					<button
+						onclick={() => {
+							goto(`/album/${page.params.api}/${album.Id}`);
+						}}
+						style="display: flex; flex-direction: column; white-space: nowrap; overflow: hidden; text-overflow: ellipsis; width:160px; height: 200px;"
+					>
+						<img
+							src={album.CoverUrl}
+							alt={album.Title}
+							style="max-width:100%; max-height:100%;"
+						/>
+						<p>{album.Title}</p>
+						{#each album.Artists as artist}
+							<a href="/artist/{page.params.api}/{artist.Id}">
+								{artist.Name}
+							</a>
+						{/each}
+					</button>
+				{/each}
+			</div>
+		</div>
+		<div>
+			<h2>EPs</h2>
+			<div style="display: flex; flex-wrap: wrap; gap: 10px;">
+				{#each artist.Ep as ep}
+					<button
+						onclick={() => {
+							goto(`/album/${page.params.api}/${ep.Id}`);
+						}}
+						style="display: flex; flex-direction: column; white-space: nowrap; overflow: hidden; text-overflow: ellipsis; width:160px; height: 200px;"
+					>
+						<img
+							src={ep.CoverUrl}
+							alt={ep.Title}
+							style="max-width:100%; max-height:100%;"
+						/>
+						<p>{ep.Title}</p>
+						{#each ep.Artists as artist}
+							<a href="/artist/{page.params.api}/{artist.Id}">
+								{artist.Name}
+							</a>
+						{/each}
+					</button>
+				{/each}
+			</div>
+		</div>
+		<div>
+			<h2>Singles</h2>
+			<div style="display: flex; flex-wrap: wrap; gap: 10px;">
+				{#each artist.Singles as single}
+					<button
+						onclick={() => {
+							goto(`/album/${page.params.api}/${single.Id}`);
+						}}
+						style="display: flex; flex-direction: column; white-space: nowrap; overflow: hidden; text-overflow: ellipsis; width:160px; height: 200px;"
+					>
+						<img
+							src={single.CoverUrl}
+							alt={single.Title}
+							style="max-width:100%; max-height:100%;"
+						/>
+						<p>{single.Title}</p>
+						{#each single.Artists as artist}
+							<a href="/artist/{page.params.api}/{artist.Id}">
+								{artist.Name}
+							</a>
+						{/each}
+					</button>
+				{/each}
+			</div>
 		</div>
 	</div>
 {/if}
