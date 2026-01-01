d08bb81d2f62c574d5adf00d73141ecc67f7bf5a
DimitriLaPoudre <lou.pellegrino@epitech.eu>
2026-01-01 22:17:31 +0100

    feat: /album better responsivness

--- client_web/src/routes/(logged)/album/[api]/[id]/+page.svelte
+++ client_web/src/routes/(logged)/album/[api]/[id]/+page.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
-	import { Download } from "lucide-svelte";
+	import { Clock4, Download } from "lucide-svelte";
 	import { afterNavigate, goto } from "$app/navigation";
 	import { page } from "$app/state";
 	import { apiFetch } from "$lib/functions/fetch";
 	import { download } from "$lib/functions/download";
-	import type { AlbumData } from "$lib/types/response";
+	import type { AlbumData, AlbumDataSong } from "$lib/types/response";
 	import { quality } from "$lib/types/quality";
 
 	let error = $state<null | string>(null);
 	let album = $state<null | AlbumData>(null);
+	let discs = $state<null | AlbumDataSong[][]>(null);
 
 	afterNavigate(async () => {
 		try {
@@ -19,6 +20,12 @@
 				throw new Error(data.error || "Failed to fetch album");
 			}
 			album = data;
+			discs = [];
+			for (const song of album.songs) {
+				if (!(song.volumeNumber in discs))
+					discs[song.volumeNumber] = [];
+				discs[song.volumeNumber][song.trackNumber] = song;
+			}
 			error = null;
 		} catch (e) {
 			error = e instanceof Error ? e.message : "Failed to load album";
@@ -28,7 +35,9 @@
 
 <svelte:head>
 	<title
-		>{album?.title || "Album"} | {album?.artists[0]?.name || "Artist"} - MusicShack</title
+		>{album?.title || "Album"} | {album?.artists
+			.map((artist) => artist.name)
+			.join(" ") || "Artist"} - MusicShack</title
 	>
 </svelte:head>
 
@@ -47,7 +56,7 @@
 			<div class="top-data">
 				<img class="cover" src={album.coverUrl} alt={album.title} />
 				<div class="data">
-					<h1>{album.title}</h1>
+					<h1 class="title">{album.title}</h1>
 					<div class="artists">
 						{#each album.artists as artist}
 							<a href="/artist/{page.params.api}/{artist.id}">
@@ -56,9 +65,12 @@
 						{/each}
 					</div>
 					<br />
-					<p>
-						{`${Math.floor(album.duration / 60)}:${(album.duration % 60).toString().padStart(2, "0")}`}
-					</p>
+					<div class="duration">
+						<Clock4 size={16} />
+						<p>
+							{`${Math.floor(album.duration / 60)}:${(album.duration % 60).toString().padStart(2, "0")}`}
+						</p>
+					</div>
 					<p>{quality[album.audioQuality]}</p>
 				</div>
 			</div>
@@ -79,47 +91,66 @@
 	</div>
 
 	<!-- page body -->
-	<div class="body">
-		{#each album.songs as song}
-			<div class="song">
-				<button
-					class="data"
-					onclick={(e) => {
-						if (
-							e.target instanceof Element &&
-							e.target.closest("a")
-						)
-							return;
-						goto(`/song/${page.params.api}/${song.id}`);
-					}}
-				>
-					<p class="number">{song.trackNumber}</p>
-					<p>{song.title}</p>
-					<div class="artists">
-						{#each song.artists as artist}
-							<a href="/artist/{page.params.api}/{artist.id}">
-								{artist.name}
-							</a>
-						{/each}
-					</div>
-					<p class="duration">
-						{`${Math.floor(song.duration / 60)}:${(song.duration % 60).toString().padStart(2, "0")}`}
-					</p>
-				</button>
-				<button
-					class="download"
-					onclick={async () => {
-						error = await download({
-							api: page.params.api!,
-							type: "song",
-							id: song!.id,
-							quality: "",
-						});
-					}}
-				>
-					<Download />
-				</button>
-			</div>
+	<div class="discs">
+		{#each discs as disc, i}
+			{#if disc !== undefined}
+				<div class="disc">
+					<h1>Disc {i}</h1>
+					{#each disc as song}
+						{#if song !== undefined}
+							<div class="item">
+								<button
+									class="song"
+									onclick={(e) => {
+										if (
+											e.target instanceof Element &&
+											e.target.closest("a")
+										)
+											return;
+										goto(
+											`/song/${page.params.api}/${song.id}`,
+										);
+									}}
+								>
+									<p class="number">{song.trackNumber}</p>
+									<div class="data">
+										<p class="title">{song.title}</p>
+										<div class="artists">
+											{#each song.artists as artist}
+												<a
+													href="/artist/{page.params
+														.api}/{artist.id}"
+												>
+													{artist.name}
+												</a>
+											{/each}
+										</div>
+									</div>
+									<div class="duration">
+										<Clock4 size={16} />
+										<p>
+											{`${Math.floor(song.duration / 60)}:${(song.duration % 60).toString().padStart(2, "0")}`}
+										</p>
+									</div>
+								</button>
+								<button
+									class="download"
+									onclick={async () => {
+										error = await download({
+											api: page.params.api!,
+											type: "song",
+											id: song!.id,
+											quality: "",
+										});
+									}}
+								>
+									<Download />
+								</button>
+							</div>
+						{/if}
+					{/each}
+				</div>
+			{/if}
 		{/each}
 	</div>
 {/if}
@@ -163,10 +194,19 @@
 					flex-direction: column;
 					gap: 7px;
 
+					.title {
+						font-weight: bolder;
+					}
 					.artists {
+						font-style: italic;
 						display: flex;
 						flex-wrap: wrap;
-						gap: 0px 0.5rem;
+						gap: 0.25rem 0.5rem;
+					}
+					.duration {
+						display: flex;
+						align-items: center;
+						gap: 0.25rem;
 					}
 				}
 			}
@@ -177,43 +217,66 @@
 		}
 	}
 
-	.body {
-		display: grid;
-		gap: 10px;
-		padding: 0 0 0 5px;
+	.discs {
+		display: flex;
+		flex-direction: column;
+		gap: 2rem;
 
-		.song {
+		.disc {
 			display: grid;
-			grid-template-columns: 1fr auto;
-			gap: 8px;
+			gap: 10px;
+			padding: 0 0 0 5px;
 
-			.data {
+			.item {
 				display: grid;
-				grid-template-columns: auto 1fr 1fr auto;
-				align-items: center;
+				grid-template-columns: 1fr auto;
 				gap: 8px;
-				border: none;
 
-				.artists {
-					display: flex;
-					gap: 1rem;
-					overflow: hidden;
+				.song {
+					display: grid;
+					grid-template-columns: auto 1fr auto;
+					align-items: center;
+					gap: 8px;
+					border: none;
 
-					a {
-						white-space: nowrap;
-						overflow-x: hidden;
-						text-overflow: ellipsis;
+					.data {
+						display: flex;
+						flex-direction: row;
+						flex-wrap: wrap;
+						justify-content: center;
+						align-items: center;
+						width: 100%;
+						gap: 1rem;
+
+						.title {
+							font-weight: bolder;
+							flex: 0 0 49%;
+						}
+						.artists {
+							font-style: italic;
+							flex: 0 0 49%;
+							display: flex;
+							flex-wrap: wrap;
+							justify-content: center;
+							align-items: center;
+							gap: 0.25rem 0.5rem;
+						}
+					}
+					.duration {
+						display: flex;
+						align-items: center;
+						gap: 0.25rem;
 					}
 				}
-			}
-			.data:hover {
-				outline: 1px solid #ffffff;
-				outline-offset: -1px;
-				background-color: inherit;
-				color: inherit;
-			}
-			.download {
-				aspect-ratio: 1/1;
+				.song:hover {
+					outline: 1px solid #ffffff;
+					outline-offset: -1px;
+					background-color: inherit;
+					color: inherit;
+				}
+				.download {
+					height: auto;
+				}
 			}
 		}
 	}
