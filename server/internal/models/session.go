c3c2479057403acfdb853b3556cbe892ff12092b
DimitriLaPoudre <lou.pellegrino@epitech.eu>
2026-01-15 06:05:07 +0100

    fix: UserSession token now indexed

--- server/internal/models/session.go
+++ server/internal/models/session.go
@@ -5,6 +5,6 @@ import "time"
 type UserSession struct {
 	ID        uint   `gorm:"primaryKey"`
 	UserId    uint   `gorm:"not null;index"`
-	Token     string `gorm:"size:64"`
+	Token     string `gorm:"size:64;uniqueIndex"`
 	ExpiresAt time.Time
 }
