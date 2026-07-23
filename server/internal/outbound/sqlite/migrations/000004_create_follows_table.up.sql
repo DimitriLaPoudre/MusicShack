b6fd854a3771608401fc93b149eea7dd9e2bb24b
DimitriLaPoudre <lou.pellegrino@epitech.eu>
2026-07-23 14:42:48 +0200

    feat(follow): migration

---
+++ server/internal/outbound/sqlite/migrations/000004_create_follows_table.up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE follows (
+	id TEXT PRIMARY KEY,
+	user_id TEXT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+	provider TEXT NOT NULL,
+	artist_id TEXT NOT NULL,
+	artist_name TEXT NOT NULL,
+	artist_picture TEXT NOT NULL,
+	featuring BOOLEAN NOT NULL,
+	UNIQUE (user_id, provider, artist_id)
+);
